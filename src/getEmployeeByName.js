e4c4f4454ffb5658e42f9c869fd859554ac0d9e2
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-25 17:22:44 -0300

    Terceiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  const achaFuncionario = data.employees.find((nome) => employeeName.includes(nome.firstName) // Lembrar que o 'nome' é um parametro criado que irá atuar dentro de employess. Usei o includes para fazer a comparação.
+  || employeeName.includes(nome.lastName));
+  return achaFuncionario;
 }
-
+console.log(getEmployeeByName('bethea')); // Obs: Lembrar que o includes é case sensitive
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,6 +5,7 @@ function getSpeciesByIds(...ids) {
   return data.species.filter((specie) => ids.includes(specie.id)); // Filtrando os Ids de species com base no id passado
 }
 
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce')); // Testando a função rsrs
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+  '89be95b3-47e4-4c5b-b687-1fabf2afa274')); // Testando a função rsrs
 
 module.exports = getSpeciesByIds;
