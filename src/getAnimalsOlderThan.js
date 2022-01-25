19f20c609010790b58f11d9882c269eb2887305f
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-25 16:27:35 -0300

    Segundo requisito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // Tinha feito a linha a baixo separada em duas partes, decidi juntar as duas, puxando direto {species} no lugar de data.
+  return species.find((test) => test.name === animal).residents.every((test) => test.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
