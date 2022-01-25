ec79ebf8685a0aac606248c38b175a836a6126e4
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-25 15:08:08 -0300

    Primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return []; // Retornando o array vazio.
+  return data.species.filter((specie) => ids.includes(specie.id)); // Filtrando os Ids de species com base no id passado
 }
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce')); // Testando a função rsrs
 
 module.exports = getSpeciesByIds;
