413847275b0acbdc37c5e243f138f3658d4ec4a9
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-26 14:54:54 -0300

    Começando o quinto requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (animal === undefined) {
+    species.forEach((index) => {
+      obj[index.name] = index.residents.length;
+    });
+    // const nomes = data.species.map((index) => index.name);
+    // const total = data.species.map((index) => index.residents.length);
+    // for (let i = 0; i < nomes.length; i += 1) {
+    //   obj[nomes[i]] = total[i];
+    return obj;
+  }
+  if (animal.sex === undefined) {
+    return species.residents.length;
+  }
 }
 
+console.log(countAnimals());
 module.exports = countAnimals;
