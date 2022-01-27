56052ff7f527c3ed45ed03d52c0656ad9cc25146
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-27 12:31:42 -0300

    Finalizando o requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,11 +1,12 @@
 const { species, hours } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 function agendaSemana(dia) {
-  const animais = species.filter((index) => index.availability.includes(dia)); // Filter vai me retornar o objeto completo
-  const animaisArray = animais.map((index) => index.name); // Fiz o map para retornar um unico array de nome de animais
+  const animais = species.filter((index) => index.availability.includes(dia)); // Filter vai me retornar o objeto completo com base no dia da semana passado.
+  const animaisDisponiveis = animais.map((index) => index.name); // Fiz o map para retornar um unico array de nome de animais, já com o availability filtrado
   if (dia === 'Monday') return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' }; // Condição de Monday
   return { officeHour: `Open from ${hours[dia].open}am until ${hours[dia].close}pm`,
-    exhibition: animaisArray }; // Objeto de horario e animais dos outros dias
+    exhibition: animaisDisponiveis }; // Objeto de horario e animais dos outros dias
 }
 
 // console.log(agendaSemana('Wednesday')); // Consolando dessa forma ele não retorna exatamente da forma esperada, mas retorna alguma coisa rsrs
@@ -20,13 +21,20 @@ const objSemana = {
   Monday: agendaSemana('Monday'),
 };
 
+const animaisNome = species.map((index) => index.name); // Fiz o map para retornar um unico array de nome de animais
+console.log(animaisNome);
+
 function getSchedule(scheduleTarget) {
-  if (!scheduleTarget) return objSemana;
-  if (scheduleTarget === 'Monday') return { [scheduleTarget]: Object.values(objSemana)[6] };
-  if (scheduleTarget !== species.name
-  || scheduleTarget !== Object.keys(objSemana)) return objSemana;
+  if (!scheduleTarget) return objSemana; // Não passando nada é retornada a agenda completa
+  if (Object.keys(hours).some((index) => scheduleTarget === index)) {
+    return { [scheduleTarget]: objSemana[scheduleTarget] }; // Com base no dia passado é retornado o respectivo value de objSemana. testes 3 e 4.
+  }
+  if (animaisNome.some((index) => scheduleTarget === index)) {
+    return species.find((index) => index.name === scheduleTarget).availability; // Após a condição de igualdade com base na const animaisNome, é retornado o availability, com base no nome do animal passado. teste 5.
+  }
+  return objSemana; // Condição para retornar a agenda caso o parametro não seja um animal e um dia. teste 2.
 }
 
-console.log(getSchedule('Monday'));
+console.log(getSchedule('bears'));
 
 module.exports = getSchedule;
