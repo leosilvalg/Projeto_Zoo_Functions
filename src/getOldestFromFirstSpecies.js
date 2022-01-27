e25b9b341965fc81dc4e91a5b820127c326b7914
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-26 22:10:09 -0300

    Terminando o nono requisito

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,8 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const func = data.employees.find((index) => id === index.id).responsibleFor[0];
-  return func;
+  const animal = species.find((index) => func.includes(index.id)).residents;
+  const maisVelho = animal.sort((a, b) => b.age - a.age)[0];
+  return Object.values(maisVelho);
 }
 console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
