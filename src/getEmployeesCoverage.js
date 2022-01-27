2770f21b8e0559b0b5669c0487eecd1973eb0f22
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-27 16:24:41 -0300

    Começando o requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function employee(pessoa) {
+  return data.employees.find((index) => pessoa.name === index.firstName
+  || pessoa.name === index.lastName || pessoa.id === index.id);
 }
+
+// function especie(employe) {
+//   return employe.responsibleFor.map((id) => data.species.find((specie) =>
+//     specie.id === id));
+// }
+
+function getEmployeesCoverage(pessoa) {
+  if (!pessoa) return;
+  return {
+    id: employee(pessoa).id,
+    fullName: `${employee(pessoa).firstName} ${employee(pessoa).lastName}`,
+    // species:
+    // locations:
+  };
+}
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
