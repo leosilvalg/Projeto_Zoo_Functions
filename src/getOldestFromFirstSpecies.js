70675948be3ac7926aa021196c89285f8ec25c4a
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-26 21:29:57 -0300

    Começando o nono requisito. Achando o primeiro animal

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const func = data.employees.find((index) => id === index.id).responsibleFor[0];
+  return func;
 }
-
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
