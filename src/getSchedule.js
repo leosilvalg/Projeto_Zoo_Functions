2496860d637461ec92cd7e4d92de3a3e6bf84842
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-27 09:45:40 -0300

    Começando a nona. retornando uma lista de animais com base no dia passado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,5 +1,13 @@
 const data = require('../data/zoo_data');
 
+function agendaSemana(dia) {
+  const animais = data.species.filter((index) => index.availability.includes(dia));
+  const animaisNome = animais.map((index) => index.name);
+  return animaisNome;
+}
+
+console.log(agendaSemana('Wednesday'));
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
 }
