96212473e4fb166bd44e3158e87e91b255583f28
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-01-27 23:56:55 -0300

    O décimo passou. Vou tentar refatorar pois o código está bem feio

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,23 +1,43 @@
 const data = require('../data/zoo_data');
 
 function employee(pessoa) {
-  return data.employees.find((index) => pessoa.name === index.firstName
+  const nomeID = data.employees.find((index) => pessoa.name === index.firstName // Pelo find, vou ter como retorno o objeto com base no primeiro elemento que satisfaça as condições passadas
   || pessoa.name === index.lastName || pessoa.id === index.id);
+  const objetoFuncionario = {
+    id: nomeID.id,
+    fullName: `${nomeID.firstName} ${nomeID.lastName}`,
+    species: ((nomeID.responsibleFor.map((id) => data.species
+      .find((specie) => specie.id === id)))).map((element) => element.name),
+    locations: ((nomeID.responsibleFor.map((id) => data.species
+      .find((specie) => specie.id === id)))).map((element) => element.location),
+  };
+  return objetoFuncionario;
 }
 
-// function especie(employe) {
-//   return employe.responsibleFor.map((id) => data.species.find((specie) =>
-//     specie.id === id));
-// }
+function todos() {
+  const todosFuncionarios = data.employees.map((index) => ({
+    id: index.id,
+    fullName: `${index.firstName} ${index.lastName}`,
+    species: ((index.responsibleFor.map((id) => data.species
+      .find((specie) => specie.id === id)))).map((element) => element.name),
+    locations: ((index.responsibleFor.map((id) => data.species
+      .find((specie) => specie.id === id)))).map((element) => element.location),
+  }));
+  return todosFuncionarios;
+}
 
-function getEmployeesCoverage(pessoa) {
-  if (!pessoa) return;
-  return {
-    id: employee(pessoa).id,
-    fullName: `${employee(pessoa).firstName} ${employee(pessoa).lastName}`,
-    // species:
-    // locations:
-  };
+function validacao(pessoa) {
+  const test = data.employees.find((index) => pessoa.name === index.firstName // Pelo find, vou ter como retorno o objeto com base no primeiro elemento que satisfaça as condições passadas
+  || pessoa.name === index.lastName || pessoa.id === index.id);
+  return test;
+}
+
+function getEmployeesCoverage(parametro) {
+  if (parametro) {
+    if (validacao(parametro)) return employee(parametro);
+    throw new Error('Informações inválidas');
+  }
+  if (!parametro) return todos();
 }
 
 console.log(getEmployeesCoverage({ name: 'Sharonda' }));
