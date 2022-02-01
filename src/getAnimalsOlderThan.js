a0f95cae560ee310b48d2623dc786d52dab284a7
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-01 00:08:22 -0300

    Adicionando arrow function no segundo requisito e melhorando os parametros das HOFs

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,7 @@
 const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // Tinha feito a linha a baixo separada em duas partes, decidi juntar as duas, puxando direto {species} no lugar de data.
-  return species.find((test) => test.name === animal).residents.every((test) => test.age >= age);
-}
+const getAnimalsOlderThan = (animal, age) => species.find((specie) => specie.name === animal)
+  .residents.every((resident) => resident.age >= age);
 
 module.exports = getAnimalsOlderThan;
