e02c141602f9e7c19a9979009cc5af2633ff0d11
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-01 00:11:40 -0300

    Adicionando arrow function no terceiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+const getEmployeeByName = (employeeName) => {
   if (!employeeName) return {};
-  const achaFuncionario = data.employees.find((nome) => employeeName.includes(nome.firstName) // Lembrar que o 'nome' é um parametro criado que irá atuar dentro de employess. Usei o includes para fazer a comparação.
+  const achaFuncionario = data.employees.find((nome) => employeeName.includes(nome.firstName)
   || employeeName.includes(nome.lastName));
   return achaFuncionario;
-}
-console.log(getEmployeeByName('Bethea')); // Obs: Lembrar que o includes é case sensitive.
+};
+
+console.log(getEmployeeByName('Bethea')); // => Lembrar que o includes é case sensitive.
 module.exports = getEmployeeByName;
