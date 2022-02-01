59ff1ce214539426af9d4a35068bdd3a1d7b9848
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-01 00:02:46 -0300

    Adicionando arrow function no primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  if (ids.length === 0) return []; // Retornando o array vazio.
-  return data.species.filter((specie) => ids.includes(specie.id)); // Filtrando os Ids de species com base no id passado
-}
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) return [];
+  return data.species.filter((specie) => ids.includes(specie.id));
+};
 
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
-  '89be95b3-47e4-4c5b-b687-1fabf2afa274')); // Testando a função rsrs
+  '89be95b3-47e4-4c5b-b687-1fabf2afa274'));
 
 module.exports = getSpeciesByIds;
