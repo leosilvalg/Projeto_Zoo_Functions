65f29f6df38742cd9d78575354aa1e7956db4fdd
Leonardo Goncalves <leonardorc.geo@gmail.com>
2022-02-01 00:31:19 -0300

    Adicionando arrow function no sexto requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,14 +1,14 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
+const countEntrants = (entrants) => {
   const child = entrants.filter((index) => index.age < 18).length;
   const adult = entrants.filter((index) => index.age >= 18 && index.age < 50).length;
   const senior = entrants.filter((index) => index.age >= 50).length;
 
   return { child, adult, senior };
-}
+};
 
-function calculateEntry(entrants) {
+const calculateEntry = (entrants) => {
   if (entrants === undefined || Object.keys(entrants).length === 0) return 0; // Refatorei depois de ler a postagem https://stackoverflow.com/questions/40615844/why-undefined-is-not-equal-to-zero-in-javascript
   const quantidade = countEntrants(entrants);
   const totalChild = quantidade.child * data.prices.child;
@@ -16,7 +16,7 @@ function calculateEntry(entrants) {
   const totalSenior = quantidade.senior * data.prices.senior;
   const somaTotal = totalAdult + totalChild + totalSenior;
   return somaTotal;
-}
+};
 
 const entrants = [{ age: 6 }, { age: 20 }, { age: 50 }, { age: 10 }];
 
